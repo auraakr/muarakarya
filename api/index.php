ad33dab59eb325cc522f3c45b63740ea6d9ff08d
Nur Azizah S <nurazizah9310@gmail.com>
2026-07-17 10:41:14 +0700

    Setup Vercel Serverless

--- api/index.php
+++ api/index.php
@@ -1,2 +1,9 @@
 <?php
+
+// Trik agar Vercel bisa menulis file cache/view sementara
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 require __DIR__ . '/../public/index.php';
